c96c7713a806b4da94ab4699bac33df9561146a4
lakshyayhih <patellakshy43@gmail.com>
2026-02-03 22:09:35 +0530

    Added Functionality on deleting record from local storage as well as from ui

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var todoarray=JSON.parse(localStorage.getItem("todo"))|| [];
 document.querySelector("form").addEventListener("submit",myTODO);
 
 displaytodo(todoarray);
+function deleterow(index){
+    todoarray.splice(index,1);
+    localStorage.setItem("todo",JSON.stringify(todoarray));
+    displaytodo(todoarray)
+
+
+}
 
 function myTODO(){
     event.preventDefault();
@@ -19,8 +26,10 @@ function myTODO(){
 
 function displaytodo(data){
     document.querySelector("tbody").innerText="";
-    data.forEach(elem => {
+    data.forEach(function (elem,ind){
     var tr=document.createElement("tr")
+    var td0=document.createElement("td");
+    td0.innerText=ind+1;
 
     var td1=document.createElement("td");
     td1.innerText=elem.tn;
@@ -28,8 +37,13 @@ function displaytodo(data){
     td2.innerText=elem.ds;
     var td3=document.createElement("td");
     td3.innerText=elem.pr;
-
-    tr.append(td1,td2,td3);
+    var td4=document.createElement("td");
+    td4.innerText="Delete"
+    //td4.style.color="red";
+    td4.innerHTML='<i class="fa-regular fa-trash-can"></i>';
+    td4.style.alignItems="center";
+    td4.addEventListener("click",function(){deleterow(ind)})
+    tr.append(td0,td1,td2,td3,td4);
 
     document.querySelector("tbody").append(tr);
 
